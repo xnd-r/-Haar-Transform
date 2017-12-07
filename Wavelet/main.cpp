500fd4d987dd276ec91d9bd68efcee1a87fce833
x-ndr <al.romanov2012@yandex.ru>
2017-12-07 23:54:43 +0300

    init_2

---
+++ Wavelet/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, void* argv[]) 
+{
+	//I'll use Haar wavelet
+	//and sin(omega* t) as a continious input signal
+	//need to code Series, coefficients and an integral
+	//terrible problems with all scope 
+
+
+
+	return 0;
+}
